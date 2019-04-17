f437bb4e705c33cba0e735499b709ea762721e6e
liyasthomas <liyascthomas@gmail.com>
2019-04-17 22:09:20 +0530

    :zap: Updated Workbox

--- sw.js
+++ sw.js
@@ -1,34 +1,34 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js');
 if (workbox) {
-	console.log("Yay! Workbox is loaded 🎉");
+	console.log(`Yay! Workbox is loaded 🎉`);
 } else {
-	console.log("Boo! Workbox didn't load 😬");
+	console.log(`Boo! Workbox didn't load 😬`);
 }
 workbox.routing.registerRoute(
 	new RegExp('.*\.js'),
-	workbox.strategies.networkFirst()
+	new workbox.strategies.NetworkFirst()
 );
 workbox.routing.registerRoute(
-	// Cache CSS files
-	/.*\.css/,
-	// Use cache but update in the background ASAP
-	workbox.strategies.staleWhileRevalidate({
-		// Use a custom cache name
+	// Cache CSS files.
+	/\.css$/,
+	// Use cache but update in the background.
+	new workbox.strategies.StaleWhileRevalidate({
+		// Use a custom cache name.
 		cacheName: 'css-cache',
 	})
 );
 workbox.routing.registerRoute(
-	// Cache image files
-	/.*\.(?:png|jpg|jpeg|svg|gif)/,
-	// Use the cache if it's available
-	workbox.strategies.cacheFirst({
-		// Use a custom cache name
+	// Cache image files.
+	/\.(?:png|jpg|jpeg|svg|gif)$/,
+	// Use the cache if it's available.
+	new workbox.strategies.CacheFirst({
+		// Use a custom cache name.
 		cacheName: 'image-cache',
 		plugins: [
       new workbox.expiration.Plugin({
-				// Cache only 20 images
+				// Cache only 20 images.
 				maxEntries: 20,
-				// Cache for a maximum of a week
+				// Cache for a maximum of a week.
 				maxAgeSeconds: 7 * 24 * 60 * 60,
 			})
     ],
