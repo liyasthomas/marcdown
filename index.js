4ff504491677fb2a031517fefe1be7b112871dd4
liyasthomas <liyascthomas@gmail.com>
2019-04-17 20:06:49 +0530

    :sparkles: Added option to open & save files from local drive

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function keyup() {
 	var mark = document.getElementById('getm').value;
 	var regex = /\s+/gi;
-	if (mark) {
+	if (mark != '') {
 		document.getElementById('content').innerHTML = marked(mark);
 		var wordCount = document.getElementById('content').innerText.trim().replace(regex, ' ').split(' ').length;
 		document.getElementById('wordcount').innerHTML = wordCount + " words";
+		document.getElementById('save').disabled = false;
 	} else {
 		document.getElementById('content').innerHTML = "";
 		document.getElementById('wordcount').innerHTML = "";
+		document.getElementById('save').disabled = true;
 	}
 }
-
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 function switchTheme(e) {
@@ -21,3 +22,27 @@ function switchTheme(e) {
 	}
 }
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+function download() {
+	var text = document.getElementById("getm").value;
+	text = text.replace(/\n/g, "\r\n");
+	var blob = new Blob([text], {
+		type: "text/plain"
+	});
+	var anchor = document.createElement("a");
+	anchor.download = "marcdown.md";
+	anchor.href = window.URL.createObjectURL(blob);
+	anchor.target = "_blank";
+	anchor.style.display = "none";
+	document.body.appendChild(anchor);
+	anchor.click();
+	document.body.removeChild(anchor);
+}
+var openFile = function (e) {
+	var input = e.target;
+	var reader = new FileReader();
+	reader.onload = function () {
+		document.getElementById('getm').value = reader.result;
+	};
+	reader.readAsText(input.files[0]);
+};
