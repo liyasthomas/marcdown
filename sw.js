34c9b86948e0758b0852038e838e767d404a7b95
liyasthomas <liyascthomas@gmail.com>
2019-05-25 00:14:04 +0530

    :package: Workbox > v4.3.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 if (workbox) {
 	console.log(`Yay! Workbox is loaded 🎉`);
 } else {
