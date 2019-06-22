40f1288f9774893087d3266095c64cefa61cb588
liyasthomas <liyascthomas@gmail.com>
2019-06-22 09:56:16 +0530

    :sparkles: Added install PWA banner button

--- index.js
+++ index.js
@@ -182,3 +182,41 @@ const preview = (e) => {
 			break
 	}
 }
+let pwaInstalled = localStorage.getItem('pwaInstalled') == 'yes'
+if (window.matchMedia('(display-mode: standalone)').matches) {
+	localStorage.setItem('pwaInstalled', 'yes')
+	pwaInstalled = true
+}
+if (window.navigator.standalone === true) {
+	localStorage.setItem('pwaInstalled', 'yes')
+	pwaInstalled = true
+}
+if (pwaInstalled) {
+	document.getElementById('installPWA').style.display = 'none'
+} else {
+	document.getElementById('installPWA').style.display = 'inline-flex'
+}
+let deferredPrompt = null
+window.addEventListener('beforeinstallprompt', (e) => {
+	deferredPrompt = e
+})
+async function installPWA() {
+	if (deferredPrompt) {
+		deferredPrompt.prompt()
+		deferredPrompt.userChoice.then(({
+			outcome
+		}) => {
+			if (outcome === 'accepted') {
+				console.log('Your PWA has been installed')
+			} else {
+				console.log('User chose to not install your PWA')
+			}
+			deferredPrompt = null
+		})
+	}
+}
+window.addEventListener('appinstalled', (evt) => {
+	localStorage.setItem('pwaInstalled', 'yes')
+	pwaInstalled = true
+	document.getElementById('installPWA').style.display = 'none'
+})
