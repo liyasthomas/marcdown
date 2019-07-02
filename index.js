b5e195080a408a9e439a5981fe426c92f9b2a30b
liyasthomas <liyascthomas@gmail.com>
2019-07-03 03:17:53 +0530

    :sparkles: Added MathJax support
    :bug: Fixes #4

--- index.js
+++ index.js
@@ -1,3 +1,153 @@
+MathJax.Hub.Config({
+	showProcessingMessages: false,
+	tex2jax: {
+		inlineMath: [['$', '$'], ['\\(', '\\)']]
+	},
+	TeX: {
+		equationNumbers: {
+			autoNumber: "AMS"
+		}
+	}
+})
+marked.setOptions({
+	renderer: new marked.Renderer(),
+	gfm: true,
+	tables: true,
+	breaks: false,
+	pedantic: false,
+	sanitize: false,
+	smartLists: true,
+	smartypants: false
+})
+const Preview = {
+	delay: 50,
+	preview: null,
+	buffer: null,
+	timeout: null,
+	mjRunning: false,
+	oldText: null,
+	Init() {
+		this.preview = document.getElementById('viewer')
+		this.buffer = document.getElementById('buffer')
+		this.textarea = document.getElementById('getm')
+	},
+	SwapBuffers() {
+		let buffer = this.preview
+		let preview = this.buffer
+		this.buffer = buffer
+		this.preview = preview
+		buffer.style.display = 'none'
+		preview.style.display = ''
+	},
+	Update() {
+		if (this.timeout) {
+			clearTimeout(this.timeout)
+		}
+		this.timeout = setTimeout(this.callback, this.delay)
+	},
+	CreatePreview() {
+		Preview.timeout = null
+		if (this.mjRunning) return
+		let text = this.textarea.value
+		if (text === this.oldtext) return
+		text = this.Escape(text)
+		this.buffer.innerHTML = this.oldtext = text
+		this.mjRunning = true
+		MathJax.Hub.Queue(
+['Typeset', MathJax.Hub, this.buffer],
+['PreviewDone', this],
+['resetEquationNumbers', MathJax.InputJax.TeX]
+		)
+	},
+	PreviewDone() {
+		this.mjRunning = false
+		text = this.buffer.innerHTML
+		text = this.PartialDescape(text)
+		this.buffer.innerHTML = marked(text)
+		let mark = document.getElementById('getm').value
+		let viewer = document.getElementById('viewer')
+		let wordcount = document.getElementById('wordcount')
+		let charcount = document.getElementById('charcount')
+		let save = document.getElementById('save')
+		let regex = /\s+/gi
+		if (mark !== '') {
+			let wordCount = viewer.innerText.trim().replace(regex, ' ').split(' ').length
+			let charCount = viewer.innerText.replace(regex, '').length
+			wordcount.innerHTML = `${wordCount} words`
+			charcount.innerHTML = `${charCount} chars`
+			save.disabled = false
+			document.querySelectorAll('code').forEach((block) => {
+				hljs.highlightBlock(block)
+			})
+			mouseUp()
+		} else {
+			viewer.innerHTML = ''
+			wordcount.innerHTML = '0 words'
+			charcount.innerHTML = '0 chars'
+			save.disabled = true
+		}
+		this.SwapBuffers()
+	},
+	Escape(html, encode) {
+		return html
+			.replace(!encode ? /&(?!#?\w+;)/g : /&/g, '&amp;')
+			.replace(/</g, '&lt;')
+			.replace(/>/g, '&gt;')
+			.replace(/"/g, '&quot;')
+			.replace(/'/g, '&#39;')
+	},
+	PartialDescape(html) {
+		let lines = html.split('\n')
+		let out = ''
+		// is true when we are
+		// ```
+		//  inside a code block
+		// ```
+		let inside_code = false
+		for (let i = 0; i < lines.length; i++) {
+			// a hack to properly rendre the blockquotes
+			if (lines[i].startsWith('&gt;')) {
+				lines[i] = lines[i].replace(/&gt;/g, '>')
+			}
+			// rendrer properly stuff like this
+			// ```c
+			//  if (a > b)
+			// ```
+			if (inside_code) {
+				// inside the code we descape stuff
+				lines[i] = lines[i]
+					.replace(/&lt;/g, '<')
+					.replace(/&gt;/g, '>')
+					.replace(/&quot;/g, '"')
+					.replace(/&#39;/g, '\'')
+			}
+			if (lines[i].startsWith('```')) {
+				inside_code = !inside_code
+			}
+			out += `${lines[i]}\n`
+		}
+		return out
+	},
+	// The idea here is to perform fast updates.
+	// See http://stackoverflow.com/questions/11228558/let-pagedown-and-mathjax-work-together/21563171?noredirect=1#comment46869312_21563171
+	// But our implementation is a bit buggy: flickering, bad rendering when someone types very fast.
+	//
+	// If you want to enable such buggy fast updates, you should
+	// add something like  onkeypress="Preview.UpdateKeyPress(event)" to textarea's attributes.
+	UpdateKeyPress({
+		keyCode
+	}) {
+		if (keyCode < 16 || keyCode > 47) {
+			this.preview.innerHTML = `<p>${marked(this.textarea.value)}</p>`
+			this.buffer.innerHTML = `<p>${marked(this.textarea.value)}</p>`
+		}
+		this.Update()
+	}
+}
+Preview.callback = MathJax.Callback(['CreatePreview', Preview])
+Preview.callback.autoReset = true
+Preview.Init()
+Preview.Update()
 const mouseUp = () => {
 	let mark = document.getElementById('getm')
 	let lineno = document.getElementById('lineno')
@@ -6,41 +156,16 @@ const mouseUp = () => {
 	lineno.innerHTML = `Line ${textLines.length}`
 	colno.innerHTML = `Col ${textLines[textLines.length - 1].length}`
 }
-const keyUp = () => {
-	let mark = document.getElementById('getm').value
-	let viewer = document.getElementById('viewer')
-	let wordcount = document.getElementById('wordcount')
-	let charcount = document.getElementById('charcount')
-	let save = document.getElementById('save')
-	let regex = /\s+/gi
-	if (mark !== '') {
-		viewer.innerHTML = marked(mark)
-		let wordCount = viewer.innerText.trim().replace(regex, ' ').split(' ').length
-		let charCount = viewer.innerText.replace(regex, '').length
-		wordcount.innerHTML = `${wordCount} words`
-		charcount.innerHTML = `${charCount} chars`
-		save.disabled = false
-		document.querySelectorAll('code').forEach((block) => {
-			hljs.highlightBlock(block)
-		})
-		mouseUp()
-	} else {
-		viewer.innerHTML = ''
-		wordcount.innerHTML = '0 words'
-		charcount.innerHTML = '0 chars'
-		save.disabled = true
-	}
-}
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]')
 const switchTheme = ({
 	target
 }) => {
 	if (target.checked) {
 		document.documentElement.setAttribute('data-theme', 'dark')
-		document.querySelector("meta[name=theme-color]").setAttribute("content", "#090821")
+		document.querySelector('meta[name=theme-color]').setAttribute('content', '#090821')
 	} else {
 		document.documentElement.setAttribute('data-theme', 'light')
-		document.querySelector("meta[name=theme-color]").setAttribute("content", "#DAE5ED");
+		document.querySelector('meta[name=theme-color]').setAttribute('content', '#DAE5ED')
 	}
 }
 toggleSwitch.addEventListener('change', switchTheme, false)
@@ -66,7 +191,7 @@ let openFile = ({
 	let reader = new FileReader()
 	reader.onload = () => {
 		document.getElementById('getm').value = reader.result
-		keyUp()
+		Preview.Update()
 		input.value = ''
 	}
 	reader.readAsText(input.files[0])
@@ -162,9 +287,9 @@ const apply = (e) => {
 		myField.selectionEnd = endPos + myValueBefore.length
 		myField.focus()
 	}
-	keyUp()
+	Preview.Update()
 }
-const preview = (e) => {
+const slide = (e) => {
 	let viewer = document.getElementById('viewer')
 	let mark = document.getElementById('getm')
 	switch (e) {
